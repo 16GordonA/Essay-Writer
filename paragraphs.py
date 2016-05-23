1bc90794729edc95334cc2da2665461c4f444ce2
16gordona <16GordonA@users.noreply.github.com>
2016-05-23 17:49:51 -0400

    Update paragraphs.py

--- paragraphs.py
+++ paragraphs.py
@@ -1,3 +1,5 @@
 /**
+Takes paragraph as text string, parses for quotes, searches internet to find full quote and verifies with citation
+
 Several different types of paragraphs which will be formed using the inputted quotes
 */
