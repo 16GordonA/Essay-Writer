181fcf47d52ff900f00b7d4b1935f71fa12b2ae7
16gordona <16GordonA@users.noreply.github.com>
2016-05-24 11:46:53 -0400

    Update fileparse.py

--- fileparse.py
+++ fileparse.py
@@ -8,6 +8,7 @@ text = []
 
 while(line is not ""):
   line = f.readLine()
-  text += [line[:len(line)-2]] #-2 is to remove \n from line end
+  if line is not "\n":
+    text += [line[:len(line)-2]] #-2 is to remove \n from line end
 
 print(text)
